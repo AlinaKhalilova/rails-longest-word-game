02a58d14123e75cceca90942f5687d7c3204c743
AlinaKhalilova <akhalilova@alumni.escaet.fr>
2018-02-12 16:23:49 +0100

    exo2 : Generating a new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,18 @@
 class GamesController < ApplicationController
   def new
+    @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }
+      @start_time = Time.now
   end
 
   def score
+    @letters = params[:letter]
+   start_time = params[:start_time].to_datetime
+
+   end_time = Time.now
+   @time_result = (end_time - start_time)
+
+
   end
+
+
 end
